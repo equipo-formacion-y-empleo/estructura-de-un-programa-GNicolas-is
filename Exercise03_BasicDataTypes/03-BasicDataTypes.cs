a6f976bcbee6cb4b1afed9af85aa2d9473e6d436
GNicolas-is <bvasquez22c@gmail.com>
2025-10-03 19:58:07 +0200

    Ejecicio1-5 resueltos

--- Exercise03_BasicDataTypes/03-BasicDataTypes.cs
+++ Exercise03_BasicDataTypes/03-BasicDataTypes.cs
@@ -1,11 +1,28 @@
+using System.Reflection.PortableExecutable;
+
 void DemoBasicDataTypes()
 {
-    throw new NotImplementedException();
+    int old = 25;
+    float class_Note = 7.6f;
+    double account = 23.45;
+    char initial = 'C';
+    string name = "Nicolas";
+    bool is_Estudent = true;
+    Console.WriteLine($"Old: {old}");
+    Console.WriteLine($"Clase Note: {class_Note}");
+    Console.WriteLine($"Account: {account}");
+    Console.WriteLine($"Initial: {initial}");
+    Console.WriteLine($"Name: {name}");
+    Console.WriteLine($"Is Estudent: {is_Estudent}");
 }
 
 void ShowDataTypeSizes()
 {
-    throw new NotImplementedException();
+    Console.WriteLine($"int: {sizeof(int)} bites");
+    Console.WriteLine($"float: {sizeof(float)} bites");
+    Console.WriteLine($"double: {sizeof(double)} bites");
+    Console.WriteLine($"char: {sizeof(char)} bites");
+    Console.WriteLine($"bool: {sizeof(bool)} bites");
 }
 
 DemoBasicDataTypes();
